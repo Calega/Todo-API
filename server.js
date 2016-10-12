42a69e112d68d0881398ccee30cc47fd438d71a1
Lucas Oliveira <lcalegari17@gmail.com>
2016-10-12 00:05:45 -0300

    Adding TODO items

--- server.js
+++ server.js
@@ -2,10 +2,32 @@ var express = require('express');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
+//todo model
+var todos = [{
+	id: 1,
+	description: 'Meet mom lunch',
+	completed: false
+}, {
+	id:2,
+	description: 'Go to market',
+	completed: false
+}, {
+	id:3,
+	description: 'Go to airport',
+	completed: true	
+}
+
+];
 
 app.get('/', function(req,res){
 	res.send('TODO Api Root');
 });
+
+//get all todo items get /todos
+app.get('/todos', function(req,res){
+	res.json(todos); //return todos converted to json
+});
+//get individual todo by id GET /todos/:id
 
 app.listen(PORT, function(){
 	console.log('Express listening on PORT ' + PORT + '!');
