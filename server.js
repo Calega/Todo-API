dfbbee3c742f679d52281b2246022c1b1a550734
Lucas Oliveira <lcalegari17@gmail.com>
2016-10-13 01:05:57 -0300

    Convert DELETE /todos:id to sequelize

--- server.js
+++ server.js
@@ -28,15 +28,17 @@ app.get('/todos', function(req, res) {
 		where.completed = false;
 	}
 
-	if(query.hasOwnProperty('q') && query.q.length > 0) {
+	if (query.hasOwnProperty('q') && query.q.length > 0) {
 		where.description = {
 			$like: '%' + query.q + '%'
 		};
 	}
 
-	db.todo.findAll({where:where}).then(function (todos) {
+	db.todo.findAll({
+		where: where
+	}).then(function(todos) {
 		res.json(todos);
-	}, function (e) {
+	}, function(e) {
 		res.status(500).send();
 	})
 });
@@ -70,16 +72,22 @@ app.post('/todos', function(req, res) {
 //DELETE /todos/:id
 app.delete('/todos/:id', function(req, res) {
 	var todoId = parseInt(req.params.id, 10);
-	var matchedTodo = _.findWhere(todos, {
-		id: todoId
-	});
 
-	if (!matchedTodo) {
-		res.status(404).json("error : No todo found with that id");
-	} else {
-		todos = _.without(todos, matchedTodo);
-		res.json(matchedTodo);
-	}
+	db.todo.destroy({
+		where: {
+			id: todoId
+		}
+	}).then(function(rowsDeleted) {
+		if (rowsDeleted === 0) {
+			res.status(404).json({
+				error: 'No todo with id'
+			});
+		} else {
+			res.status(204).send();
+		}
+	}, function(error) {
+		res.status(500).send();
+	})
 });
 
 //PUT /todos/:id
