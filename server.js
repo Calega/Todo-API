47feacb9d0498fa0c86455984665d161e27bacf0
Lucas Oliveira <lcalegari17@gmail.com>
2016-10-13 00:13:57 -0300

    Convert GET /todos to sequelize

--- server.js
+++ server.js
@@ -19,26 +19,26 @@ app.get('/', function(req, res) {
 
 //GET /todos?complete=true&q=work(eg:house, work, job)
 app.get('/todos', function(req, res) {
-	var queryParams = req.query; //Query is an object to specify URL parameters
-	var filteredTodos = todos;
+	var query = req.query; //Query is an object to specify URL parameters
+	var where = {};
 
-	if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'true') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: true
-		});
-	} else if (queryParams.hasOwnProperty('completed') && queryParams.completed === 'false') {
-		filteredTodos = _.where(filteredTodos, {
-			completed: false
-		});
+	if (query.hasOwnProperty('completed') && query.completed === 'true') {
+		where.completed = true;
+	} else if (query.hasOwnProperty('completed') && query.completed === 'false') {
+		where.completed = false;
 	}
 
-	if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-		filteredTodos = _.filter(filteredTodos, function(todo) {
-			return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-		});
+	if(query.hasOwnProperty('q') && query.q.length > 0) {
+		where.description = {
+			$like: '%' + query.q + '%'
+		};
 	}
 
-	res.json(filteredTodos); //return todos converted to json
+	db.todo.findAll({where:where}).then(function (todos) {
+		res.json(todos);
+	}, function (e) {
+		res.status(500).send();
+	})
 });
 
 //GET /todos/:id
