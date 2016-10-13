6d455eb6abcf1e80ddd8e0085cb857de76222e4c
Lucas Oliveira <lcalegari17@gmail.com>
2016-10-13 10:06:35 -0300

    Add authenticate class method

--- server.js
+++ server.js
@@ -138,27 +138,15 @@ app.post('/users/login', function(req, res) {
 	var body = _.pick(req.body, 'email', 'password');
 	var attributes = {};
 
-	if (typeof body.email !== 'string' || typeof body.password !== 'string') {
-		return res.status(400).send();
-	}
-
-	db.user.findOne({
-		where: {
-			email: body.email
-		}
-	}).then(function(user) {
-		if (!user || !bcrypt.compareSync(body.password, user.get('password_hash'))) {
-			return res.status(401).send();
-		}
-
-		res.json(user.toPublicJSON()); //Public Json return only the fields we want to expose
+	//creating authenticate class method to do stuff for authentication (created in the model)
+	db.user.authenticate(body).then(function(user) {
+		res.json(user.toPublicJSON());
 	}, function(e) {
-		res.status(500).send();
+		res.status(401).send();
 	});
-
 });
 
-db.sequelize.sync().then(function() {
+db.sequelize.sync({force:true}).then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on PORT ' + PORT + '!');
 	});
